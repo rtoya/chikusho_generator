481730e34c10aefd1346f501b860ca45226af994
rtoya <toya@ig-partners.co.jp>
2013-10-14 15:12:26 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChikushoGenerator::Application.config.secret_token = '1031c52401ed4b63824f225c97d6b4ecd4650337e18785ef0f72b8cba022df58d5f211a1b537b0afc5b23278ea07910e39bb78b84458516d7a7b2a56d6819184'
