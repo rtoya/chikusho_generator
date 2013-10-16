0a1e2e32143f14319a828db884b190bac5ec07d6
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-16 11:50:58 +0900

    generated top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ChikushoGenerator::Application.routes.draw do
+
+  get "home/index"
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
