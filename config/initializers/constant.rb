dd939ced1d0cdc8a5d18e453c4f0ff387c3675cb
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-16 17:27:36 +0900

    フレーム画面のガワを作成した

---
+++ config/initializers/constant.rb
@@ -0,0 +1,1 @@
+CHIKUSHO_FLAME = ['chikusho_flame1_normal.png', 'chikusho_flame1_white.png', 'chikusho_flame1_black.png', 'chikusho_flame2_normal.png', 'chikusho_flame2_white.png', 'chikusho_flame2_black.png']
