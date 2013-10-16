250e8c69afb0fe56104bb607d396208be1a9957a
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-16 19:25:47 +0900

    フレーム選択画面を実装した

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.flame_thumb').on('click', function(){
+    var _self = $(this);
+    _self.closest('div.row').find('a.selected').removeClass('selected');
+    _self.addClass('selected');
+    var flame_id = _self.attr("id").replace(/flame/g, '');
+    var next_btn = $('a.next_btn');
+    next_btn.attr('href', '/home/adjust_photo?fid=' + flame_id);
+    next_btn.removeAttr('disabled');
+  });
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
 class HomeController < ApplicationController
 
-  def select_flame
-
-  end
-
   def adjust_photo
-
+    # fidというパラメータでフレームの識別IDが飛んでくる
+    # このIDは、CHIKUSHO_FLAMESという定数（配列）のインデックス
+    # CHIKUSHO_FLAMES[params[:fid].to_i]
+    # => こんな感じでフレーム画像ファイル名が取り出せる
   end
 
   def show_gallery
-
   end
 
 end
