75c20ea77ef798f1e2e30d5711c575e703c4f959
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-18 01:42:28 +0900

    chaned validation of image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,6 +11,6 @@ class Image < ActiveRecord::Base
 
   scope :is_public_ok, ->{ where(public_flg:true) }
 
-  validates_attachment_size :input, :less_than => 5.megabytes
+  validates_attachment_size :input, :less_than => 10.megabytes
   validates_attachment_content_type :input, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 end
