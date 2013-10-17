8e75ecc47a46453af283e152cbf1f89bcf0ce55c
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-17 23:25:56 +0900

    done migrating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017034851) do
+ActiveRecord::Schema.define(version: 20131017124838) do
 
   create_table "images", force: true do |t|
     t.boolean  "public_flg"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20131017034851) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "random_hash"
+    t.integer  "flame_id"
+    t.binary   "output_data"
   end
 
 end
