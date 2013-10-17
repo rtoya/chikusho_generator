5546764ece584cefd8c726b6101443f04d3f90bd
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-17 12:33:01 +0900

    アップロード画面のview, jsを修正した

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.flame_thumb').on('click', function(){
     var _self = $(this);
-    _self.closest('div.row').find('input.selected').removeClass('selected');
+    _self.closest('div.row').find('a.selected').removeClass('selected');
     _self.addClass('selected');
     var flame_id = _self.attr("id").replace(/flame/g, '');
     var next_btn = $('input.next_btn');
