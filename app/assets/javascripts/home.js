6db79eb35b862da30716c53df9faab364e1d0453
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-10-17 17:55:48 +0900

    edited adjust page view

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+var photo_original_width = 0;
+var photo_original_height = 0;
+
+$(function(){
   $('.flame_thumb').on('click', function(){
     var _self = $(this);
     _self.closest('div.row').find('a.selected').removeClass('selected');
@@ -8,4 +11,23 @@ $(document).ready(function(){
     $('input#flame_id').val(flame_id);
     next_btn.removeAttr('disabled');
   });
+
+  $('#jcrop_target').Jcrop({
+    aspectRatio: 1.2,
+    minSize: [30, 25],
+    onSelect: showPreview,
+    onChange: showPreview
+  });
 });
+var showPreview = function(coords){
+  photo_original_width = $('#jcrop_target').width();
+  photo_original_height = $('#jcrop_target').height();
+  var rx = 420 / coords.w;
+  var ry = 350 / coords.h;
+  $('.preview').css({
+    width: Math.round(rx * photo_original_width) + 'px',
+    height: Math.round(ry * photo_original_height) + 'px',
+    marginLeft: '-' + Math.round(rx * coords.x) + 'px',
+    marginTop: '-' + Math.round(ry * coords.y) + 'px'
+  });
+}
