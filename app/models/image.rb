9f6a13d402581c4789c3a33c00390a7f608bc242
rtoya <toya@ig-partners.co.jp>
2013-10-18 14:07:06 +0900

    fix_bug

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,6 +9,7 @@ class Image < ActiveRecord::Base
   validates :random_hash, presence: true
 
   scope :is_public_ok, ->{ where(public_flg:true) }
+  scope :have_output, ->{ where('output_data is not null') }
 
   validates_attachment_size :input, :less_than => 10.megabytes
   validates_attachment_content_type :input, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
